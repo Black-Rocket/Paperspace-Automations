053046a69b1272f6e169c4d0bd0ec75eb8c07f6a
jlargs64 <justinlargo7@gmail.com>
2019-07-18 17:40:56 -0400

    Working on security csurf protection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
@@ -32,6 +34,17 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Add in the csurf and cookie parser
+app.use(cookieParser());
+// csurf needs cookieparser and express session to be useful
+// so we import and use those first
+app.use(csurf());
+
+app.use(function(req, res, next) {
+  res.locals.csrftoken = req.csrfToken();
+  next();
+});
+
 // get routes
 const routes = require('./Routes/routes');
 const profileRoutes = require('./Routes/profile');
@@ -54,9 +67,7 @@ db.once('open', function() {
   // we're connected!
   console.log('Database connection successful!');
 
-  /**
-   * Start the server on port 3000 or another port.
-   */
+  // Start the server
   app.listen(port, () => {
     console.log('Server started on port ' + port + '.');
   });
